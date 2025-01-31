5851ff72691397852277c49a8a651f1a5e92f555
julius_el_gringo <julien.rousselot@hotmail.fr>
2025-01-31 12:30:48 +0100

    gestion erreurs

--- script2.js
+++ script2.js
@@ -37,6 +37,7 @@ function calcul(){
     catch(err){
         if(err instanceof SyntaxError){
             console.log("Votre calcul est impossible!");
+            result2.textContent = "Votre calcul est impossible!";
         }
     }
 }
